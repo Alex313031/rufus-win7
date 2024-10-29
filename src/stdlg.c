d770df349cbd67617693eca546c265e74e3a4a60
Alexander Frick <Alex313031@gmail.com>
2024-10-29 00:41:48 -0500

    fix

--- src/stdlg.c
+++ src/stdlg.c
@@ -1445,7 +1445,7 @@ void SetFidoCheck(void)
 		return;
 	}
 
-	if (nWindowsVersion < WINDOWS_8) {
+	if (WindowsVersion.Version < WINDOWS_8) {
 		ubprintf("Notice: The ISO download feature has been deactivated because "
 			"your version of Windows is too old.");
 		return;
